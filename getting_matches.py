135963b7cfb4536782e528d2831662ca96772cea
omidelmi87 <87409582+omidelmi87@users.noreply.github.com>
2022-10-10 22:45:35 -0500

    Create getting_matches.py

---
+++ getting_matches.py
@@ -0,0 +1,17 @@
+import urllib.request
+import os
+import schedule
+import time
+from datetime import date
+import datetime
+
+with open("API_Token.txt") as API_Keys:
+  API_KEY = API_keys.read()
+  
+url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=" + API_KEY + "&format=json&steamid=" + user_id[i] + "&include_appinfo=1&include_played_free_games=1&appids_filter="
+try:
+  req = urllib.request.Request(url)
+  response = urllib.request.urlopen(req)
+  urlbyte = response.read()
+except:
+  time.sleep(10)
